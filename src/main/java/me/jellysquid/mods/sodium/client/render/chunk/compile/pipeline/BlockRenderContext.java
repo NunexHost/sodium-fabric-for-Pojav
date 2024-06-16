6c83d366024b020a3da14b15cff183e31c18babd
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-06-15 23:24:37 -0300

    Update BlockRenderContext.java

--- src/main/java/me/jellysquid/mods/sodium/client/render/chunk/compile/pipeline/BlockRenderContext.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/chunk/compile/pipeline/BlockRenderContext.java
@@ -7,65 +7,72 @@ import net.minecraft.util.math.BlockPos;
 import org.joml.Vector3f;
 import org.joml.Vector3fc;
 
-import java.util.Arrays;
-
 public class BlockRenderContext {
     private final WorldSlice world;
 
-    private final BlockPos.Mutable[] posPool;
-    private final Vector3f[] originPool;
-    private int posIndex = 0;
-    private int originIndex = 0;
+    private final BlockPos.Mutable pos = new BlockPos.Mutable();
+
+    private final Vector3f origin = new Vector3f();
 
     private BlockState state;
     private BakedModel model;
+
     private long seed;
 
-    public BlockRenderContext(WorldSlice world, int poolSize) {
+
+    public BlockRenderContext(WorldSlice world) {
         this.world = world;
-        posPool = new BlockPos.Mutable[poolSize];
-        originPool = new Vector3f[poolSize];
-        for (int i = 0; i < poolSize; i++) {
-            posPool[i] = new BlockPos.Mutable();
-            originPool[i] = new Vector3f();
-        }
     }
 
     public void update(BlockPos pos, BlockPos origin, BlockState state, BakedModel model, long seed) {
+        this.pos.set(pos);
+        this.origin.set(origin.getX(), origin.getY(), origin.getZ());
+
         this.state = state;
         this.model = model;
+
         this.seed = seed;
-
-        BlockPos.Mutable nextPos = posPool[posIndex];
-        nextPos.set(pos);
-        posIndex = (posIndex + 1) % posPool.length;
-
-        Vector3f nextOrigin = originPool[originIndex];
-        nextOrigin.set(origin.getX(), origin.getY(), origin.getZ());
-        originIndex = (originIndex + 1) % originPool.length;
     }
 
+    /**
+     * @return The position (in world space) of the block being rendered
+     */
     public BlockPos pos() {
-        return posPool[posIndex];
+        return this.pos;
     }
 
+    /**
+     * @return The world which the block is being rendered from
+     */
     public WorldSlice world() {
-        return world;
+        return this.world;
     }
 
+    /**
+     * @return The state of the block being rendered
+     */
     public BlockState state() {
-        return state;
+        return this.state;
     }
 
+    /**
+     * @return The model used for this block
+     */
     public BakedModel model() {
-        return model;
+        return this.model;
     }
 
+    /**
+     * @return The origin of the block within the model
+     */
     public Vector3fc origin() {
-        return originPool[originIndex];
+        return this.origin;
     }
 
+    /**
+     * @return The PRNG seed for rendering this block
+     */
     public long seed() {
-        return seed;
+        return this.seed;
     }
 }
