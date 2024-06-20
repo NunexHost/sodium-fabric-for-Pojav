35b04642de608a659f463960412ae8c0e3380aba
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-06-20 09:43:22 -0300

    Update DoubleBufferedQueue.java

--- src/main/java/me/jellysquid/mods/sodium/client/util/collections/DoubleBufferedQueue.java
+++ src/main/java/me/jellysquid/mods/sodium/client/util/collections/DoubleBufferedQueue.java
@@ -6,108 +6,99 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Arrays;
 
 public final class DoubleBufferedQueue<E> {
-    private QueueImpl<E> read, write;
+    private static final int INITIAL_CAPACITY = 256;
+    private static final int MAX_CAPACITY = 1 << 30; // Max array size as a power of two
+
+    private E[] readElements, writeElements;
+    private int readIndex, writeIndex;
 
     public DoubleBufferedQueue() {
-        this.read = new QueueImpl<>();
-        this.write = new QueueImpl<>();
+        this.readElements = createArray(INITIAL_CAPACITY);
+        this.writeElements = createArray(INITIAL_CAPACITY);
     }
 
     public boolean flip() {
-        if (this.write.size() == 0) {
+        if (writeIndex == 0) {
             return false;
         }
 
-        var tmp = this.read;
-        this.read = this.write;
-        this.write = tmp;
+        E[] tmp = readElements;
+        readElements = writeElements;
+        writeElements = tmp;
 
-        this.write.clear();
-
+        writeIndex = 0;
         return true;
     }
 
     public void reset() {
-        this.read.clear();
-        this.write.clear();
+        readIndex = 0;
+        writeIndex = 0;
     }
 
     public ReadQueue<E> read() {
-        return this.read;
+        return new QueueView<>(readElements, readIndex, writeIndex);
     }
 
     public WriteQueue<E> write() {
-        return this.write;
+        return new QueueView<>(writeElements, writeIndex, writeElements.length);
     }
 
-    private static final class QueueImpl<E> implements ReadQueue<E>, WriteQueue<E> {
-        private E[] elements;
-        private int readIndex, writeIndex;
+    private static class QueueView<E> implements ReadQueue<E>, WriteQueue<E> {
+        private final E[] elements;
+        private final int startIndex, endIndex;
 
-        QueueImpl() {
-            this(256);
-        }
-
-        @SuppressWarnings("unchecked")
-        QueueImpl(int capacity) {
-            this.elements = (E[]) new Object[capacity];
+        QueueView(E[] elements, int startIndex, int endIndex) {
+            this.elements = elements;
+            this.startIndex = startIndex;
+            this.endIndex = endIndex;
         }
 
         @Override
         public void ensureCapacity(int numElements) {
-            int len = this.writeIndex + numElements;
-
-            if (len > this.elements.length) {
-                this.grow(len);
+            // Check if more capacity is needed and resize if necessary
+            if (startIndex + numElements > elements.length) {
+                grow(startIndex + numElements);
             }
         }
 
         @Override
-        public @Nullable E dequeue() {
-            if (this.readIndex == this.writeIndex) {
+        public E dequeue() {
+            if (startIndex >= endIndex) {
                 return null;
             }
-
-            return this.elements[this.readIndex++];
+            return elements[startIndex++];
         }
 
         @Override
-        public void enqueue(@NotNull E e) {
-            if (this.writeIndex >= this.elements.length) {
-                this.resize(this.writeIndex + 1);
-            }
-
-            this.elements[this.writeIndex++] = e;
+        public void enqueue(E e) {
+            elements[endIndex++] = e;
         }
 
-
+        @Override
         public void clear() {
-            if (this.writeIndex != 0) {
-                Arrays.fill(this.elements, 0, this.writeIndex, null);
-            }
-
-            this.readIndex = 0;
-            this.writeIndex = 0;
+            Arrays.fill(elements, startIndex, endIndex, null);
+            startIndex = 0;
+            endIndex = 0;
         }
 
+        @Override
         public int size() {
-            return this.writeIndex - this.readIndex;
+            return endIndex - startIndex;
         }
 
-        private void grow(int minimumSize) {
-            this.resize(getNextSize(minimumSize, this.elements.length));
+        private void grow(int minCapacity) {
+            int newCapacity = Math.min(MAX_CAPACITY, getNextPowerOfTwo(minCapacity));
+            elements = Arrays.copyOf(elements, newCapacity);
         }
 
-        private void resize(int length) {
-            @SuppressWarnings("unchecked")
-            E[] elements = (E[]) new Object[length];
-            System.arraycopy(this.elements, 0, elements, 0, this.writeIndex);
-
-            this.elements = elements;
+        private static int getNextPowerOfTwo(int n) {
+            int highestOneBit = Integer.highestOneBit(n);
+            return (highestOneBit == n) ? n : highestOneBit << 1;
         }
+    }
 
-        private static int getNextSize(int minimumSize, int currentSize) {
-            return Math.max(minimumSize, currentSize << 1);
-        }
+    @SuppressWarnings("unchecked")
+    private static <E> E[] createArray(int capacity) {
+        return (E[]) new Object[capacity];
     }
 }
