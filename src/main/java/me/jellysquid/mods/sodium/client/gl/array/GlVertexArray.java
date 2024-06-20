134e066c06307465a26239e406d30688d4e035f6
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-06-19 22:42:28 -0300

    Update GlVertexArray.java

--- src/main/java/me/jellysquid/mods/sodium/client/gl/array/GlVertexArray.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gl/array/GlVertexArray.java
@@ -1,37 +1,39 @@
 package me.jellysquid.mods.sodium.client.gl.array;
 
-import javax.microedition.khronos.opengles.GL10;
+import me.jellysquid.mods.sodium.client.gl.GlObject;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL15;
 
 /**
- * Provides Vertex Array functionality on supported platforms.
+ * Provides Vertex Array functionality on supported platforms (OpenGL 2.0).
  */
-public class GlVertexArray {
-    public static final int NULL_ARRAY_ID = 0;
-    private int handle;
+public class GlVertexArray extends GlObject {
+    private int vboId;
 
     public GlVertexArray() {
-        handle = createVertexArray();
-    }
-
-    public int getHandle() {
-        return handle;
-    }
-
-    private int createVertexArray() {
-        int[] array = new int[1];
-        GLES20.glGenVertexArrays(1, array, 0);
-        return array[0];
+        // Generate a Vertex Buffer Object (VBO) for vertex data
+        this.vboId = GL15.glGenBuffers();
     }
 
     public void bind() {
-        GLES20.glBindVertexArray(handle);
+        // Bind the VBO when needed (before drawing)
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, this.vboId);
     }
 
     public void unbind() {
-        GLES20.glBindVertexArray(NULL_ARRAY_ID);
+        // Unbind the VBO
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+    }
+
+    public void setData(float[] data) {
+        // Upload vertex data to the VBO
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, this.vboId);
+        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, data, GL15.GL_STATIC_DRAW);
+        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
     }
 
     public void delete() {
-        GLES20.glDeleteVertexArrays(1, new int[] { handle }, 0);
+        // Delete the VBO when no longer needed
+        GL15.glDeleteBuffers(this.vboId);
     }
 }
