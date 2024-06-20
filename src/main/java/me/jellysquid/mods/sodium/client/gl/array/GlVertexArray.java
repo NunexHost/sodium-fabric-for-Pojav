ae56b161f76571554b887ff35ca45db5ed092913
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-06-19 22:31:27 -0300

    Update GlVertexArray.java

--- src/main/java/me/jellysquid/mods/sodium/client/gl/array/GlVertexArray.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gl/array/GlVertexArray.java
@@ -1,41 +1,46 @@
 package me.jellysquid.mods.sodium.client.gl.array;
 
+import me.jellysquid.mods.sodium.client.gl.GlObject;
 import org.lwjgl.opengl.GL11;
 
 /**
- * Provides Vertex Array functionality using OpenGL 2.0 mechanisms.
+ * Provides Vertex Array functionality emulation on OpenGL 2.0
  */
-public class GlVertexArray {
-    // No handle needed for OpenGL 2.0, as VAOs aren't available
+public class GlVertexArray extends GlObject {
+    public static final int NULL_ARRAY_ID = 0;
+
+    private int currentVbo = -1; // Track the currently bound VBO
 
     public GlVertexArray() {
-        // No need to generate an ID, as VAOs aren't used in OpenGL 2.0
+        // Not required for emulating VAOs in OpenGL 2.0
+    }
+
+    @Override
+    public void bind() {
+        // No binding needed for emulated VAOs
+    }
+
+    @Override
+    public void unbind() {
+        // No unbinding needed for emulated VAOs
+    }
+
+    @Override
+    public void delete() {
+        // No object to delete for emulated VAOs
     }
 
     /**
-     * Binds a vertex buffer to a specific attribute index.
-     *
-     * @param attribute the attribute index (e.g., GL_VERTEX_ATTRIB_ARRAY_0)
-     * @param vbo the vertex buffer object containing the data
-     * @param stride the number of bytes between consecutive vertex elements (or 0 for tightly packed data)
-     * @param offset the byte offset within the buffer to start reading from
+     * Binds a vertex buffer to this emulated vertex array.
+     * 
+     * @param vboId The ID of the vertex buffer object to bind
      */
-    public static void bindVertexBuffer(int attribute, long vbo, int stride, int offset) {
-        GL11.glBindBuffer(GL11.GL_ARRAY_BUFFER, (int) vbo);
-        GL11.glVertexAttribPointer(attribute, GL11.GL_FLOAT, stride, offset);
-    }
-
-    /**
-     * Enables or disables a vertex attribute array.
-     *
-     * @param attribute the attribute index (e.g., GL_VERTEX_ATTRIB_ARRAY_0)
-     * @param enabled whether to enable (true) or disable (false) the attribute
-     */
-    public static void enableAttribute(int attribute, boolean enabled) {
-        if (enabled) {
-            GL11.glEnableVertexAttribArray(attribute);
-        } else {
-            GL11.glDisableVertexAttribArray(attribute);
+    public void bindVertexBuffer(int vboId) {
+        if (currentVbo != vboId) {
+            GL11.glBindBuffer(GL11.GL_ARRAY_BUFFER, vboId);
+            currentVbo = vboId;
         }
     }
+
+    // You can add similar methods for binding other vertex attribute types (e.g., element arrays)
 }
