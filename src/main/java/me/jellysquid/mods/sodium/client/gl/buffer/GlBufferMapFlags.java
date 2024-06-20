8e6dac8e6a8e7ed6f00bb0948153ea0a1c0c859f
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-06-19 22:54:10 -0300

    Update GlBufferMapFlags.java

--- src/main/java/me/jellysquid/mods/sodium/client/gl/buffer/GlBufferMapFlags.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gl/buffer/GlBufferMapFlags.java
@@ -1,19 +1,13 @@
 package me.jellysquid.mods.sodium.client.gl.buffer;
 
 import me.jellysquid.mods.sodium.client.gl.util.EnumBit;
-import org.lwjgl.opengl.GL30C;
-import org.lwjgl.opengl.GL33C;
-import org.lwjgl.opengl.GL44C;
 
 public enum GlBufferMapFlags implements EnumBit {
-    READ(GL30C.GL_MAP_READ_BIT),
-    WRITE(GL30C.GL_MAP_WRITE_BIT),
-    PERSISTENT(GL44C.GL_MAP_PERSISTENT_BIT),
-    INVALIDATE_BUFFER(GL30C.GL_MAP_INVALIDATE_BUFFER_BIT),
-    INVALIDATE_RANGE(GL30C.GL_MAP_INVALIDATE_RANGE_BIT),
-    EXPLICIT_FLUSH(GL30C.GL_MAP_FLUSH_EXPLICIT_BIT),
-    COHERENT(GL44C.GL_MAP_COHERENT_BIT),
-    UNSYNCHRONIZED(GL33C.GL_MAP_UNSYNCHRONIZED_BIT);
+    READ(0x0001), // GL_READ_ONLY_ARB
+    WRITE(0x0002), // GL_WRITE_ONLY_ARB
+    INVALIDATE_BUFFER(0x0008), // GL_INVALIDATE_BUFFER
+    INVALIDATE_RANGE(0x0004), // GL_INVALIDATE_RANGE
+    UNSYNCHRONIZED(0x0020); // GL_MAP_UNSYNCHRONIZED_BIT is not available in OpenGL 2.0
 
     private final int bit;
 
